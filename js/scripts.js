ca48301e4c95fd807d2e2ec76ba2622a2f21d9b1
MatthewJbelcher7 <110428919+MatthewJbelcher7@users.noreply.github.com>
2023-01-18 20:50:44 -0700

    add HTML and files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Business logic
+function saySomething(whatToSay) {
+  window.alert(whatToSay);
+}
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+
+// User interface
+// const number1 = parseInt(prompt("Enter a number:"));
+// const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add(number1, number2));
